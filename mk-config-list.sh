eb818912c94568ead8780aa69f1a74091fdf66e8
Ulf Samuelsson <ulf_samuelsson@telia.com>
2011-10-23 15:45:57 +0200

    build scripts:	Update buildscripts
    Default cross_compiler = Ubuntus ARM compiler
    
    mk-config-list.sh:
    	Sourced to create "allboards.cfg"
    
    allboards.cfg:
    	This is a list of all boards with 'defconfig's.
    
    ALLCONFIGS:
    	Will "make oldconfig" on all boards, allowing
    	the user to manually select options, undefined in the
    	current defconfig, and will save the result.
    
    MAKEALL:
    	Will build all the the configurations,
    	with build status in the "result" folder.
    
    	"result" contains the following folders
    	* OK		- log files of all successful builds
    	* FAIL		- log files of all failed builds
    	* binaries	- bin files
    	* elf		- elf files
    	* map		- map files
    
    Signed-off-by: Ulf Samuelsson <ulf.samuelsson@telia.com>

---
+++ mk-config-list.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export	CONFIG_LIST=allboards.cfg
+
+mk_config ()
+{
+	rm	-f	$CONFIG_LIST
+	for path in `ls	board/*/*_defconfig` ; do
+#		echo	$path
+#		echo	`basename $path`
+		filename=`basename $path | sed s/_defconfig//g`
+		echo	"$filename" >> $CONFIG_LIST
+	done
+}
+
+mk_config
+
+# cat	$CONFIG_LIST
+
