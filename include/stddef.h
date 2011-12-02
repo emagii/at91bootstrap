5c05fc5beab8b3707e406bd01d94700b01940a36
Ulf Samuelsson <ulf@emagii.com>
2011-12-02 20:04:38 +0100

    Add some std include files
    Signed-off-by: Ulf Samuelsson <ulf@emagii.com>

---
+++ include/stddef.h
@@ -0,0 +1,7 @@
+#ifndef _STDDEF_H_
+#define _STDDEF_H_
+
+#define	NULL	((void *) 0)
+
+#endif /*_STDDEF_H_*/
+
---
+++ include/stdlib.h
@@ -0,0 +1,10 @@
+#ifndef _STDLIB_H_
+#define _STDLIB_H_
+
+typedef	struct	{
+	int	quot;
+	int	rem;
+} div_t;
+
+#endif /*_STDLIB_H_*/
+
